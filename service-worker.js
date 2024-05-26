877c3139f3cddac98747c0eca17a028984915d9b
Gilberto <gimidia@hotmail.com>
2024-05-26 09:12:38 -0300

    ajustado para host

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,46 @@
 const CACHE_NAME = 'pwa-camera-cache-v1';
 const urlsToCache = [
-  '/',
-  'index.html',
-  'manifest.json',
-  //'/style.css',
-  //'/script.js',
+  '/pwa/',
+  '/pwa/index.html',
+  '/pwa/manifest.json',
+  '/pwa/service-worker.js',
   'https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.min.js'
 ];
 
+// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
-      .catch(error => {
-        console.error('Failed to cache', error);
-      })
   );
 });
 
+// Cache and Return Requests
 self.addEventListener('fetch', event => {
-  // Verificar se a requisição é do tipo GET
-  if (event.request.method !== 'GET') {
-    return;
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
   }
+});
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
-      })
+// Update Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
